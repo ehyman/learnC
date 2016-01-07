2fdb8ba2a84f054fbabb887aac93d1ec4fdec855
hyman <elahyman@gmail.com>
2016-01-07 21:59:33 +0800

    a simple c program, print one word per line.

---
+++ chapterOne/printWord.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#define  IN  1
+#define  OUT 0
+
+void main()
+{
+	int c, t, s;
+	int state;
+
+	state = OUT;
+
+	while( (c = getchar()) != EOF)
+	{
+		if(c == ' ' || c == '\n' || c == '\t')
+		{
+			state = OUT;
+			if(t == 1)
+			{
+				printf("\n");
+			}
+			t = 0;
+		}else if(state == OUT)
+		{
+			state = IN;
+			t = 1;
+		}
+
+		if(t == 1)
+		{
+			putchar(c);
+		}
+	}
+}
