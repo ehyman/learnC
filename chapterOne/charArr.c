b58e1fe7fa4e574970a5323889505a4c214cc3d5
Hyman <elahyman@gmail.com>
2016-01-12 22:04:30 +0800

    字符数组测试

---
+++ chapterOne/charArr.c
@@ -0,0 +1,62 @@
+#include "stdio.h"
+#define MAXLINE 1000
+
+int testGetLine(char line[], int maxline);
+
+void testCopy(char to[], char from[]);
+
+int main()
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	while((len = testGetLine(line, MAXLINE)) > 0)
+	{
+		if(len > max)
+		{
+			max = len;
+			testCopy(longest, line);
+		}
+	}
+	if(max > 0)
+	{
+		printf("%s", longest);
+	}
+	return 0;
+}
+
+int testGetLine(char s[], int lim)
+{
+	int c;
+	int i;
+	for(i=0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
+	{
+		s[i] = c;
+	}
+
+	if(c == '\n')
+	{
+		s[i] = c;
+		++i;
+	}
+
+	s[i] = '\0';
+
+	return i;
+}
+
+void testCopy(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while((to[i] == from[i]) != '\0')
+	{
+		++i;
+	}
+}
+
+/* vim: set ts=4 sw=4 tw=0 noet :*/
